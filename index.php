22754a50131cd1a8786a07520413c50c0119f0e5
Leo Picado <leopic@gmail.com>
2016-02-15 00:16:01 -0600

    - Cambiando la extension del archivo a php. - Sacando el nombre del proyecto a una variable.

--- index.php
+++ index.php
@@ -1,15 +1,19 @@
+<?php
+    $nombre = "Proyecto Uno";
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Proyecto Uno: Ejemplo</title>
+    <title><?php echo($nombre); ?>: Ejemplo</title>
     <link rel="stylesheet" href="css/bootstrap.min.css">
 </head>
 <body>
 <div ng-app="proyectoUno" ng-controller="ProyectoUnoController">
     <div class="col-md-4 col-md-offset-4">
         <div class="page-header text-center">
-            <h1>Proyecto Uno: Ejemplo</h1>
+            <h1><?php echo($nombre); ?>: Ejemplo</h1>
         </div>
         <pre>{{saludo}}</pre>
     </div>
